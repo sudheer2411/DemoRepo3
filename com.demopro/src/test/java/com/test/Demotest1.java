dc2cd49bef7a91bc24f407619606277fbc5c8828
Sudhir <Sudhir@SudPC>
2023-02-21 11:54:57 +0000

    adding new test

---
+++ com.demopro/src/test/java/com/test/Demotest1.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Demotest1 {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("hello world but I am second test");
+	}
+
+}
